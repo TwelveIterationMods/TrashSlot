72b2eb880ba979a1e81e1c86ed7f4771c2bd550a
Blay09 <blay09@hotmail.de>
2015-10-24 17:02:24 +0200

    Fix server-side crash

--- src/main/java/net/blay09/mods/trashslot/net/NetworkHandler.java
+++ src/main/java/net/blay09/mods/trashslot/net/NetworkHandler.java
@@ -4,6 +4,7 @@ import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.blay09.mods.trashslot.TrashSlot;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
@@ -20,6 +21,11 @@ public class NetworkHandler {
     }
 
     public static EntityPlayer getPlayerEntity(MessageContext ctx) {
-        return ctx.side == Side.SERVER ? ctx.getServerHandler().playerEntity : Minecraft.getMinecraft().thePlayer;
+        return ctx.side == Side.SERVER ? ctx.getServerHandler().playerEntity : getClientPlayerEntity();
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static EntityPlayer getClientPlayerEntity() {
+        return Minecraft.getMinecraft().thePlayer;
     }
 }
