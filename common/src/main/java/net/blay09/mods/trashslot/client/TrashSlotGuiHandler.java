b201c25a798216dcf72c8778f2add0db9f61dc9a
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2024-10-27 19:59:36 +0100

    feat: Add Trash Item tooltip to trash slot #150

--- common/src/main/java/net/blay09/mods/trashslot/client/TrashSlotGuiHandler.java
+++ common/src/main/java/net/blay09/mods/trashslot/client/TrashSlotGuiHandler.java
@@ -244,7 +244,8 @@ public class TrashSlotGuiHandler {
                     hintMessage.withStyle(ChatFormatting.GOLD);
                     showHint(Hints.LOCKED, hintMessage, 5000, true);
                 } else {
-                    var hintMessage = Component.translatable("trashslot.hint.unlocked", ModKeyMappings.keyBindToggleSlotLock.getBinding().key().getDisplayName());
+                    var hintMessage = Component.translatable("trashslot.hint.unlocked",
+                            ModKeyMappings.keyBindToggleSlotLock.getBinding().key().getDisplayName());
                     hintMessage.withStyle(ChatFormatting.GOLD);
                     showHint(Hints.UNLOCKED, hintMessage, 5000, true);
                 }
@@ -283,6 +284,27 @@ public class TrashSlotGuiHandler {
                 poseStack.translate(((AbstractContainerScreenAccessor) screen).getLeftPos(), ((AbstractContainerScreenAccessor) screen).getTopPos(), 0);
                 AbstractContainerScreen.renderSlotHighlight(event.getGuiGraphics(), trashSlot.x, trashSlot.y, 0);
                 poseStack.popPose();
+            }
+
+            boolean isMouseSlot = ((AbstractContainerScreenAccessor) screen).callIsHovering(trashSlot, event.getMouseX(), event.getMouseY());
+            if (isMouseSlot) {
+                if (screen.getMenu().getCarried().isEmpty() && trashSlot.hasItem()) {
+                    event.getGuiGraphics().renderTooltip(Minecraft.getInstance().font, trashSlot.getItem(), event.getMouseX(), event.getMouseY());
+                } else {
+                    if (TrashSlotConfig.getActive().instantDeletion) {
+                        event.getGuiGraphics()
+                                .renderTooltip(Minecraft.getInstance().font,
+                                        Component.translatable("tooltip.trashslot.destroy_item"),
+                                        event.getMouseX(),
+                                        event.getMouseY());
+                    } else {
+                        event.getGuiGraphics()
+                                .renderTooltip(Minecraft.getInstance().font,
+                                        Component.translatable("tooltip.trashslot.trash_item"),
+                                        event.getMouseX(),
+                                        event.getMouseY());
+                    }
+                }
             }
         }
     }
