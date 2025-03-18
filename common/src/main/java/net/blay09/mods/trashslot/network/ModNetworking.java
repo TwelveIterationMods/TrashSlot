5becd2b70781622166dfff690c2e0d242afe1bf6
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-03-18 15:39:03 +0100

    fix: Mark TrashSlot as client/server-optional #154

--- common/src/main/java/net/blay09/mods/trashslot/network/ModNetworking.java
+++ common/src/main/java/net/blay09/mods/trashslot/network/ModNetworking.java
@@ -1,10 +1,13 @@
 package net.blay09.mods.trashslot.network;
 
 import net.blay09.mods.balm.api.network.BalmNetworking;
+import net.blay09.mods.trashslot.TrashSlot;
 
 public class ModNetworking {
 
     public static void initialize(BalmNetworking networking) {
+        networking.allowClientAndServerOnly(TrashSlot.MOD_ID);
+
         networking.registerServerboundPacket(MessageDeleteFromSlot.TYPE, MessageDeleteFromSlot.class, MessageDeleteFromSlot::encode, MessageDeleteFromSlot::decode, MessageDeleteFromSlot::handle);
         networking.registerServerboundPacket(MessageTrashSlotClick.TYPE, MessageTrashSlotClick.class, MessageTrashSlotClick::encode, MessageTrashSlotClick::decode, MessageTrashSlotClick::handle);
         networking.registerClientboundPacket(MessageTrashSlotContent.TYPE, MessageTrashSlotContent.class, MessageTrashSlotContent::encode, MessageTrashSlotContent::decode, MessageTrashSlotContent::handle);
