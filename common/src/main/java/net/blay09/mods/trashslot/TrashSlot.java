4d75e77b6f298fb39bb7b3e48c080d6a3418e772
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-11-27 17:15:59 +0100

    feat: Update to Minecraft 1.21.11

--- common/src/main/java/net/blay09/mods/trashslot/TrashSlot.java
+++ common/src/main/java/net/blay09/mods/trashslot/TrashSlot.java
@@ -2,13 +2,20 @@ package net.blay09.mods.trashslot;
 
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.core.BalmRegistrars;
+import net.blay09.mods.balm.platform.compatibility.recipeviewer.RecipeViewerInfoProvider;
+import net.blay09.mods.balm.platform.compatibility.recipeviewer.RecipeViewerRegistrar;
 import net.blay09.mods.balm.platform.event.callback.ServerPlayerCallback;
 import net.blay09.mods.trashslot.api.TrashSlotAPI;
+import net.blay09.mods.trashslot.client.TrashSlotGuiHandler;
 import net.blay09.mods.trashslot.network.MessageTrashSlotContent;
 import net.blay09.mods.trashslot.network.ModNetworking;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.item.ItemStack;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.Collections;
+import java.util.List;
 
 public class TrashSlot {
 
@@ -34,6 +41,16 @@ public class TrashSlot {
         ServerPlayerCallback.OpenMenu.EVENT.register((player, menu) -> {
             ItemStack trashItem = TrashHelper.getTrashItem(player);
             Balm.networking().sendTo(player, new MessageTrashSlotContent(trashItem));
+        });
+
+        Balm.modSupport().recipeViewers().register(Identifier.fromNamespaceAndPath("trashslot", "occlusions"), new RecipeViewerInfoProvider() {
+            @Override
+            public void initialize(RecipeViewerRegistrar recipeViewerRegistrar) {
+                recipeViewerRegistrar.registerGlobalScreenOcclusion(containerScreen -> {
+                    final var trashSlot = TrashSlotGuiHandler.getTrashSlotComponent();
+                    return trashSlot != null && trashSlot.isVisible() ? List.of(trashSlot.getRectangle()) : Collections.emptyList();
+                });
+            }
         });
     }
 }
