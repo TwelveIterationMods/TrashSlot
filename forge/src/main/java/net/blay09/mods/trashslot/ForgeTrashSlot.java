6811d75e987fc3acf7b4965bee9994990ad81f17
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2025-12-15 13:09:44 +0100

    feat: Update to Forge 1.21.11

--- forge/src/main/java/net/blay09/mods/trashslot/ForgeTrashSlot.java
+++ forge/src/main/java/net/blay09/mods/trashslot/ForgeTrashSlot.java
@@ -1,8 +1,8 @@
 package net.blay09.mods.trashslot;
 
 import net.blay09.mods.balm.api.Balm;
-import net.blay09.mods.balm.api.client.BalmClient;
-import net.blay09.mods.balm.forge.ForgeLoadContext;
+import net.blay09.mods.balm.client.BalmClient;
+import net.blay09.mods.balm.forge.platform.runtime.ForgeLoadContext;
 import net.blay09.mods.trashslot.client.TrashSlotClient;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
