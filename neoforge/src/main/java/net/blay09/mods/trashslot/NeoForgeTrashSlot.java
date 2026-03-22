d085a165c5491cf306dc7ebdb959ef5928e47b25
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-22 18:56:33 +0100

    feat: Update to Minecraft 26.1

--- neoforge/src/main/java/net/blay09/mods/trashslot/NeoForgeTrashSlot.java
+++ neoforge/src/main/java/net/blay09/mods/trashslot/NeoForgeTrashSlot.java
@@ -3,12 +3,13 @@ package net.blay09.mods.trashslot;
 import net.blay09.mods.balm.Balm;
 import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(TrashSlot.MOD_ID)
 public class NeoForgeTrashSlot {
 
-    public NeoForgeTrashSlot(IEventBus eventBus) {
+    public NeoForgeTrashSlot(ModContainer modContainer, IEventBus eventBus) {
         PlatformBindings.INSTANCE = new PlatformBindings() {
             @Override
             public boolean supportsKeyModifiers() {
@@ -16,7 +17,7 @@ public class NeoForgeTrashSlot {
             }
         };
 
-        final var loadContext = new NeoForgeLoadContext(eventBus);
+        final var loadContext = new NeoForgeLoadContext(modContainer, eventBus);
         Balm.initializeMod(TrashSlot.MOD_ID, loadContext, TrashSlot::initialize);
     }
 
--- neoforge/src/main/java/net/blay09/mods/trashslot/client/NeoForgeTrashSlotClient.java
+++ neoforge/src/main/java/net/blay09/mods/trashslot/client/NeoForgeTrashSlotClient.java
@@ -5,13 +5,14 @@ import net.blay09.mods.balm.neoforge.platform.runtime.NeoForgeLoadContext;
 import net.blay09.mods.trashslot.TrashSlot;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 
 @Mod(value = TrashSlot.MOD_ID, dist = Dist.CLIENT)
 public class NeoForgeTrashSlotClient {
 
-    public NeoForgeTrashSlotClient(IEventBus eventBus) {
-        final var loadContext = new NeoForgeLoadContext(eventBus);
+    public NeoForgeTrashSlotClient(ModContainer modContainer, IEventBus eventBus) {
+        final var loadContext = new NeoForgeLoadContext(modContainer, eventBus);
         BalmClient.initializeMod(TrashSlot.MOD_ID, loadContext, TrashSlotClient::initialize);
     }
 
