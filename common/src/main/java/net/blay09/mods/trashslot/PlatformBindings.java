48b0aecfd3007863455dd9a9acfe3bdfb428e852
BlayTheNinth <1933180+BlayTheNinth@users.noreply.github.com>
2026-03-26 18:21:48 +0100

    refactor: Remove obsolete classes

--- common/src/main/java/net/blay09/mods/trashslot/PlatformBindings.java
+++
@@ -1,9 +0,0 @@
-package net.blay09.mods.trashslot;
-
-public abstract class PlatformBindings {
-
-    public static PlatformBindings INSTANCE;
-
-    public abstract boolean supportsKeyModifiers();
-
-}
